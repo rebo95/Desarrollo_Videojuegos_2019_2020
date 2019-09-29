273ec9c624fb86b62b379954d9e1e01aa4cef674
rebo95 <arebored@ucm.es>
2019-09-29 21:45:57 +0200

    practica 1 updated

--- Aprendizaje automático y minería de datos/Practica1/Practica1.py
+++ Aprendizaje automático y minería de datos/Practica1/Practica1.py
@@ -1,4 +1,5 @@
-import numpy as numpy
+import numpy as np
+import matplotlib.pyplot as plt
 from pandas.io.parsers import read_csv
 
 def carga_csv(file_name):
@@ -7,3 +8,16 @@ def carga_csv(file_name):
     valores = read_csv(file_name, header=None).values
 
     return valores.astype(float) #parseamos a float (suponemos que siempre trabajaremos con float)
+
+
+def pinta_puntos():
+
+    poblacion = carga_csv('ex1data1.csv')
+
+    X = np.array(poblacion[:,0]) #sería nuestra variable independiente
+    Y = np.array(poblacion[:,1]) #sería nuestra variable independiente o el valor resultante de evaluar la función en X
+
+    plt.scatter(X,Y, alpha= 0.5)
+    plt.show()
+
+pinta_puntos()
