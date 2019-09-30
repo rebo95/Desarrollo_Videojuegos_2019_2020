18ff1a0844e4f87b5dd671fe3ee3dc72e7e6ac98
rebo95 <arebored@ucm.es>
2019-09-30 12:30:47 +0200

    Practica 1 parte 1 done

--- Aprendizaje automático y minería de datos/Practica1/Practica1.py
+++ Aprendizaje automático y minería de datos/Practica1/Practica1.py
@@ -9,15 +9,60 @@ def carga_csv(file_name):
 
     return valores.astype(float) #parseamos a float (suponemos que siempre trabajaremos con float)
 
+def hth(x, th):
+    return th[0] + th[1] * x
 
-def pinta_puntos():
+
+
+def one_variable_linear_regresion(alpha_ = 0.01, num_iter = 1500):
 
     poblacion = carga_csv('ex1data1.csv')
 
     X = np.array(poblacion[:,0]) #sería nuestra variable independiente
     Y = np.array(poblacion[:,1]) #sería nuestra variable independiente o el valor resultante de evaluar la función en X
 
-    plt.scatter(X,Y, alpha= 0.5)
-    plt.show()
+    m = len(X)
+    
+    #construimos matriz Z
+    th0 = 0
+    th1 = 0
+    Z = [th0 ,th1]
 
-pinta_puntos()
+    alpha_m = (alpha_/m)
+    
+
+    for i in range(1500):
+
+        sumatory = 0
+        sumatory1 = 0
+
+        for j in range(m):
+            sumatory += hth(X[j], Z) - Y[j]
+            sumatory1 += (hth(X[j], Z) - Y[j]) * X[j]
+
+        th0 -= alpha_m * sumatory
+        th1 -= alpha_m * sumatory1 
+
+
+        sumatory_j = 0
+        for k in range(m):
+             sumatory_j += (hth(X[k], Z) - Y[k]) ** 2
+
+        J = sumatory_j/(2*m)
+
+        #Reconstruimos Z a partir de los nuevos valores de 
+        Z = [th0, th1]
+
+        print(J)
+
+    plt.scatter(X, Y, alpha= 0.5)
+    plt.plot([5, 22], [hth(5,Z) , hth(22, Z)], color = "red")
+
+    X_axis_th0 = np.linspace(-10, 10, 20)
+    Y_axis_th1 = np.linspace(-1, 4, 20)
+
+
+    plt.show()
+    
+
+one_variable_linear_regresion()
