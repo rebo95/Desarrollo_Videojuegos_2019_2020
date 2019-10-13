e9586ceedc0c0a89532951e3ccf74e22e0b7588e
rebo95 <arebored@ucm.es>
2019-10-13 19:40:25 +0200

    AA assignment2 : atribute mappingn, cost and gradient for regularized regresion, optimus parameters an optimus function drawing implemented

--- Aprendizaje automático y minería de datos/Practica2/Practica2.py
+++ Aprendizaje automático y minería de datos/Practica2/Practica2.py
@@ -5,6 +5,7 @@ from math import e
 from pandas.io.parsers import read_csv
 
 import scipy.optimize as opt
+from sklearn.preprocessing import PolynomialFeatures
 
 
 def data_csv(file_name):
@@ -43,14 +44,25 @@ def draw_frontier(X, Y, Thetas):
     h = h.reshape(xx1.shape)
 
     plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='b')
+
+def draw_frontier_regularized(X, Y, Theta, poly):
+
+    plt.figure()
+    x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
+    x2_min, x2_max = X[:, 1].min(), X[:, 1].max()
+    xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max), np.linspace(x2_min, x2_max))
+
+    h = sigmoide_function(poly.fit_transform(np.c_[xx1.ravel(), xx2.ravel()]).dot(Theta))
+    h = h.reshape(xx1.shape)
+    plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='g')
+
     
-
-def ones_matrix(X):
-
-    return np.zeros(X.shape) + 1
 
 def H(X, Z): #Hipótesis del mocelo lineal vectorizada 
     return np.dot(X, Z)
+
+def two_power(X):
+    return X**2
 
 def sigmoide_function(X):
 
@@ -76,24 +88,45 @@ def cost(Thetas, X, Y):
     A = y_T_log_g_X_Thetas
 
     #B
-    one_g_X_Thetas = ones_matrix(g_X_Thetas) - g_X_Thetas
+    one_g_X_Thetas = 1 - g_X_Thetas
     log_one_g_X_Thetas = np.log(one_g_X_Thetas)
     T_log_one_g_X_Thetas = np.transpose(log_one_g_X_Thetas)
 
     B = T_log_one_g_X_Thetas
 
     #C
-    C = ones_matrix(g_X_Thetas) - Y
+    C = 1 - Y
 
     J = (-1/m) * (A + (np.dot(B, C)))
 
 
     return J
 
+def cost_regularized(Thetas, X, Y, h):
+
+    m = X.shape[0] 
+    Thetas_ = Thetas
+
+    #J(θ) = (cost(Thetas, X, Y)) + D
+    #J(θ) = [−(1/m) * ((log (g(Xθ)))T * y + (log (1 − g(Xθ)))T * (1 − y)))] + (λ/2m)*E(Theta^2)
+
+    cost_ = cost(Thetas, X, Y)
+   
+    #D
+    Thetas_ = two_power(Thetas_)
+
+    D = h/(2*m) * np.sum(Thetas_)
+
+    J_regularized = (cost_) + D
+
+    return J_regularized
+
 
 def gradient(Thetas, X, Y):
 
     m = X.shape[0]
+    #(δJ(θ)/δθj) =(1/m)*XT*(g(Xθ) − y)
+    
 
     X_Teta = np.dot(X, Thetas)
     g_X_Thetas = sigmoide_function(X_Teta)
@@ -104,10 +137,27 @@ def gradient(Thetas, X, Y):
 
     return gradient
 
+def gradient_regularized(Thetas, X, Y, h):
+
+    m = X.shape[0]
+    #(δJ(θ)/δθj) =(1/m)*XT*(g(Xθ) − y) + (λ/2m)(Theta)
+    gradient_ = gradient(Thetas, X, Y)
+
+    g_regularized = gradient_ + (h/m)*Thetas
+    
+    return g_regularized
+
 
 def optimized_parameters(Thetas, X, Y):
 
     result = opt.fmin_tnc(func = cost, x0 = Thetas, fprime = gradient, args = (X, Y) )
+    theta_opt = result[0]
+
+    return theta_opt
+
+def optimized_parameters_regularized(Thetas, X, Y, h):
+
+    result = opt.fmin_tnc(func = cost_regularized, x0 = Thetas, fprime = gradient_regularized, args = (X, Y, h) )
     theta_opt = result[0]
 
     return theta_opt
@@ -128,7 +178,7 @@ def logistic_regresion_evaluation(X, Y, Z):
     return(percentage_correct_clasification)
     
 
-def solve_problem():
+def logistic_regresion():
 
     X_, Y_ = data_builder(data_csv("ex2data1.csv"))
     
@@ -144,14 +194,11 @@ def solve_problem():
     cost_ = cost(Thetas, X_V, Y_)
     gradient_ = gradient(Thetas, X_V, Y_)
 
-
-
-
     #testing and comparing if the resoults are correct having in mind the resoults given in the assignment document
     print(cost_)
     print(gradient_)
 
-
+    
     optimized_thetas = optimized_parameters(Thetas, X_V, Y_)#Optimus cost is obtained by calling the cost ecuation using the optimized thetas
 
 
@@ -166,31 +213,37 @@ def solve_problem():
 
     draw_frontier(X_, Y_, optimized_thetas )
     data_visualization(X_, Y_)
-
-
-
-
-
-
     
 
+def regularized_logistic_regresion():
 
+    X_, Y_ = data_builder(data_csv("ex2data2.csv"))
+    
+    
+    poly = PolynomialFeatures(degree=6)
+    X_poly = poly.fit_transform(X_)
 
+    #theta and alpha inicialization
+    Thetas = np.zeros(X_poly.shape[1])
+    h = 1
 
+    cost_r = cost_regularized(Thetas, X_poly, Y_, h)
+    gradient_r = gradient_regularized(Thetas, X_poly, Y_, h)
+    print(cost_r)
+    print(gradient_r)
 
+    optimized_thetas_regularized = optimized_parameters_regularized(Thetas, X_poly, Y_, h)
 
-
-
-
-
-
+    draw_frontier_regularized(X_, Y_, optimized_thetas_regularized, poly )
+    data_visualization(X_, Y_)
 
 
 def test():
-    mat = np.array([[1,math.e,3],[math.e,2,1]])
-    Y = np.log(mat)
-    print(Y)
+    mat = np.array([[1,2,3],[3,2,1]])
+    mat = two_power(mat)
+    print(mat)
 
-solve_problem()
+
+regularized_logistic_regresion()
 
 
